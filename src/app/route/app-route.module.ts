147a3e61a635b5b3e9c5191ace07816638d4a603
Jimmy <jiaming0708@gmail.com>
2017-07-06 14:37:27 +0800

    Update app-route.module.ts
    remark do not use

--- src/app/route/app-route.module.ts
+++ src/app/route/app-route.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// import { CommonModule } from '@angular/common';
 import { RouterModule, Routes} from '@angular/router';
-import { TodoModule } from '.././todo/todo.module';
+// import { TodoModule } from '.././todo/todo.module';
 import { TodoComponent } from '.././todo/todo.component'
 import { HomeComponent } from "app/home/home.component";
 
@@ -14,8 +14,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    CommonModule,
-    TodoModule,
+    //不需要import這兩個module進來，因為在這個moudle底下沒有用到
+//     CommonModule,
+//     TodoModule,
     RouterModule.forRoot(routes),
   ],
   exports:[RouterModule]
